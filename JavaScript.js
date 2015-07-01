eda0f01d29d218858e0b3d8ef8f4627331bd10aa
zhyt <zhyt0520@gmail.com>
2015-07-01 17:26:04 +0800

    go on

--- JavaScript.js
+++ JavaScript.js
@@ -2,6 +2,8 @@ function $(id){
 	return document.getElementById(id)
 }
 window.onload = function(){
+
+	// 竖分隔线拖动
 	$('line').onmousedown = function(){
 		var old_width = $('left').offsetWidth;
 		var old_x = event.clientX;
@@ -18,4 +20,24 @@ window.onload = function(){
 	document.onmouseup = function(){
 		document.onmousemove = null;
 	}
+
+	// 右键菜单
+	document.oncontextmenu = function(){
+		// if(!event) event=window.event;
+		if(event.button==2){
+			var x=event.clientX;
+			var y=event.clientY;
+			$('contextmenu').style.left=x+'px';
+			$('contextmenu').style.top=y+'px';
+			$('contextmenu').style.visibility='visible';
+			$('contextmenu').style.position='fixed';
+		}
+		return false
+	}
+	document.onclick = function(){
+		$('contextmenu').style.visibility='hidden';
+	}
+
+	// 左侧目录点击后显示和隐藏
+
 }
--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function connect_db(){
 	try{
 		$conn = new PDO ($dsn,DB_USER,DB_PASSWORD);
 		// $conn = new PDO ("mysql:host=localhost;dbname=zhyt",DB_USER,DB_PASSWORD);
-		echo 'connect database successfully.';
+		// echo 'connect database successfully.';
 	}
 	catch(PDOException $e)
 	{
@@ -20,16 +20,15 @@ function dis_dir($conn){
 	$result=$conn->prepare($query);
 	$result->execute();
 	$res=$result->fetchall(PDO::FETCH_ASSOC);
-	echo count($res);
 	for($i=0;$i<count($res);$i++){
 		if($res[$i]['Depth']==0){
-			echo "<div class='dir".$i."'>".$res[$i]['Name'];
+			echo "<div class='dir0'><p>".$res[$i]['Name'].'</p>';
 			for($j=0;$j<count($res);$j++){
 				if($res[$j]['ParentId']==$res[$i]['Id']){
-					echo "<div class='dir".$j."'>".$res[$j]['Name'];
+					echo "<div class='dir1'><p>".$res[$j]['Name'].'</p>';
 					for($k=0;$k<count($res);$k++){
 						if($res[$k]['ParentId']==$res[$j]['Id']){
-							echo "<div calss='dir".$k."'>".$res[$k]['Name'];
+							echo "<div calss='dir2'><p>".$res[$k]['Name'].'</p>';
 							echo '</div>';
 						}
 					}
