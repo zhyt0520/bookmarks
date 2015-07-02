eef6475af9698322718afa77a2cb63b9a2d48288
zhyt <zhyt0520@gmail.com>
2015-07-02 21:21:21 +0800

    go on

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,9 @@
 function $(id){
 	return document.getElementById(id)
 }
+
+// 竖分隔线拖动
 window.onload = function(){
-	// 竖分隔线拖动
 	$('line').onmousedown = function(){
 		var old_width = $('left').offsetWidth;
 		var old_x = event.clientX;
@@ -19,26 +20,33 @@ window.onload = function(){
 	document.onmouseup = function(){
 		document.onmousemove = null;
 	}
-	// 右键菜单
-	document.oncontextmenu = function(){
-		// if(!event) event=window.event;
-		if(event.button==2){
-			var x=event.clientX;
-			var y=event.clientY;
-			$('contextmenu').style.left=x+'px';
-			$('contextmenu').style.top=y+'px';
-			$('contextmenu').style.visibility='visible';
-			$('contextmenu').style.position='fixed';
-		}
-		return false
-	}
-	document.onclick = function(){
-		$('contextmenu').style.visibility='hidden';
-	}
 }
+
 // 左侧目录点击后显示和隐藏子元素
-function toggle_children(self){
-		var z = self.parentNode.getElementsByTagName('div')[0].className;
+function toggle_children(this_ele){
+		var z = this_ele.parentNode.getElementsByTagName('div');
 		// var zz = z.childNodes;
 		console.log(z);
+}
+
+// 右键菜单
+document.oncontextmenu = function(){
+	// if(!event) event=window.event;
+	if(event.button==2){
+		var x=event.clientX;
+		var y=event.clientY;
+		$('contextmenu').style.left=x+'px';
+		$('contextmenu').style.top=y+'px';
+		$('contextmenu').style.visibility='visible';
+		$('contextmenu').style.position='fixed';
+	}
+	return false
+}
+document.onclick = function(){
+	$('contextmenu').style.visibility='hidden';
+}
+
+// 动态更新右侧内容
+function dis_url_ajax(){
+
 }
