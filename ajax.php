3708d52e1971086c187475ec9d2b67f5ad1dc862
zhyt <zhyt0520@gmail.com>
2015-07-18 10:46:49 +0800

    go on

--- ajax.php
+++ ajax.php
@@ -21,6 +21,8 @@ if(isset($_GET['id'])){
 // js新建右侧条目，传递参数：id为当前左侧选中dir的数据库id；name和url为新建条目的名称和网址；返回右侧conten_right的内容
 if(isset($_GET['id'],$_GET['name'],$_GET['url'])){
 	$id=$_GET['id'];
+	$name=$_GET['name'];
+	$url=$_GET['url'];
 	$conn=connect_db();
 	// ！！！存储数据需要知道当前选中目录的path,是不是用左侧dir单击函数来传递，那么需要ajax返回两个结果分别处理，怎么办
 	$query='insert into bookmarks ()';
